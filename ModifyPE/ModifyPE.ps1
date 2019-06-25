040e3a03684a5101418d329a22696392b13e0570
nmoSeptember <nmo.september@outlook.com>
2019-06-25 18:04:01 +0800

    add modify pe

---
+++ ModifyPE/ModifyPE.ps1
@@ -0,0 +1,15 @@
+﻿function ModifyPe([string]$fileName, [string]$dosStubStr)
+{
+    $fs = 
+    $bw = New-Object -TypeName System.IO.BinaryWriter([System.IO.File]::Open($fileName,[System.IO.FileMode]::Open,[System.IO.FileAccess]::Write,[System.IO.FileShare]::Write))
+    $bw.Seek(5*16-2,[System.IO.SeekOrigin]::Begin)
+    $bw.Write([System.Text.Encoding]::Default.GetBytes($dosStubStr))
+}
+
+function Main()
+{
+    $dosStub = "COMP temporary modification by aa.     ";
+    ModifyPe "D:\Workspaces\Code\temp\aadtb.dll.mui" $dosStub;
+}
+
+Main
---
+++ ModifyPE/ModifyPE/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ModifyPE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryModify(@"D:\Workspaces\Code\temp\aadtb.dll.mui");
+        }
+
+        static void BinaryModify(string fileName)
+        {
+            string str = "COMP temporary modification by aa.     ";
+
+            using (BinaryWriter bw = new BinaryWriter(File.Open(fileName, FileMode.Open, FileAccess.ReadWrite, FileShare.Write)))
+            {
+                bw.Seek(5 * 16-2, SeekOrigin.Begin);
+                bw.Write(System.Text.Encoding.Default.GetBytes(str));
+                
+            }
+        }
+    }
+}
